b815516c1cd165202174caebfb9f53f21210bef1
nulladmin1 <shrey.deogade@protonmail.com>
2024-10-20 23:11:22 -0400

    Added a class for colors, and started on a welcome message

--- frankenzork/main.py
+++ frankenzork/main.py
@@ -1,12 +1,36 @@
-from hashlib import sha256
+class AnsiiEscapes:
+    class fg:
+        black = "\033[30m"
+        white = "\033[37m"
+        red = "\033[31m"
+        yellow = "\033[33m"
+        green = "\033[32m"
+        blue = "\033[34m"
+        purple = "\033[35m"
+        cyan = "\033[36m"
+    class bg:
+        black ="\033[40m"
+        white = "\033[47m"
+        red = "\033[41m"
+        yellow = "\033[43m"
+        green = "\033[42m"
+        blue = "\033[44m"
+        purple = "\033[45m"
+        cyan = "\033[46m"
+    clear = "\033[0m"
 
-def get_sha256(unencoded: str) -> str:
-    return(sha256(unencoded.encode()).hexdigest()) 
+class WelcomeScreen():
+    def __init__(self) -> None:
+        self.message = f"""
+         {AnsiiEscapes.fg.black}█████████████{AnsiiEscapes.clear} {AnsiiEscapes.fg.green}FRANKENZORK {AnsiiEscapes.fg.red}I{AnsiiEscapes.clear}: The creation of Frankenstein's monster.
+         {AnsiiEscapes.fg.black}█████████████{AnsiiEscapes.clear} Made by {AnsiiEscapes.fg.green}nulladmin1{AnsiiEscapes.clear}: {AnsiiEscapes.fg.cyan}https://github.com/nulladmin1{AnsiiEscapes.clear} 
+         {AnsiiEscapes.fg.black}█{AnsiiEscapes.fg.green}█{AnsiiEscapes.fg.black}█{AnsiiEscapes.fg.green}█{AnsiiEscapes.fg.black}█{AnsiiEscapes.fg.green}█{AnsiiEscapes.fg.black}█{AnsiiEscapes.fg.green}█{AnsiiEscapes.fg.black}█{AnsiiEscapes.fg.green}█
+
+        """
 
 def main():
-    unencodedString = input("Enter unencoded string: ")
-    encodedString = get_sha256(unencodedString)
-    print(encodedString)
+    welcomeScreen = WelcomeScreen()
+    print(welcomeScreen.message)
 
 if __name__ == "__main__":
     main()
